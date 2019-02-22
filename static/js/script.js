572869f2bab15a93d0fb262d1c7aa2ba3e63e666
Carl Chilton <carlchilton@yahoo.co.uk>
2019-02-22 18:10:41 +0000

    Added ability to remove added inputs from the recipe

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
-    $("#ingredientsList").find(".ingredient")
+    $("body").on("click", ".removeInput", function() {
+        let currentList = $(this).closest(".form-group");
+        $(this).closest(".addedInputGroup").remove();
+        let totalInputs = $(currentList).find(".addedInputGroup").length;
+        for (i=0; i < totalInputs; i++) {
+            currentLabel = $(currentList).find(".addedInputGroup").eq(i).find("label").text();
+            currentLabel = currentLabel.substr(0, currentLabel.length - 1);
+            $(currentList).find(".addedInputGroup").eq(i).find("label").text(currentLabel + (i + 2));
+        }
+        
+    });
 });
 
 $("#addRecipeForm").on("submit", function(e) {
@@ -10,16 +20,27 @@ $("#addRecipeForm").on("submit", function(e) {
 
 function add_ingredient() {
     let totalIngredients = $("#ingredientsList").find(".form-control").length + 1;
-    $("#ingredientsList").append("<label class='stepLabel' for='ingredient" + totalIngredients + "'>Ingredient " 
+    $("#ingredientsList").append("<div class='addedInputGroup'><label class='stepLabel' for='ingredient" + totalIngredients + "'>Ingredient " 
     + totalIngredients + "</label><input id='ingredient" + totalIngredients
     + "' name='ingredient" + totalIngredients
-    + "' type='text' class='form-control'>");
+    + "' type='text' class='addedInput form-control'><div " 
+    + "class='removeInput btn btn-danger'><i class='fas fa-times'></i></div></div>");
 }
 
 function add_instruction() {
     let totalInstructions = $("#instructionsList").find(".form-control").length + 1;
-    $("#instructionsList").append("<label class='stepLabel' for='instruction" + totalInstructions + "'>Step " 
+    $("#instructionsList").append("<div class='addedInputGroup'><label class='stepLabel' for='instruction" + totalInstructions + "'>Step " 
     + totalInstructions + "</label><input id='ingredient" + totalInstructions
     + "' name='instruction" + totalInstructions
-    + "' type='text' class='form-control'>");
+    + "' type='text' class='addedInput form-control'><div " 
+    + "class='removeInput btn btn-danger'><i class='fas fa-times'></i></div></div>");
+    
 }
+
+// function removeInput(element) {
+//     console.log(element);
+//     // console.log("YAY");
+//     // console.log($(this).closest(".addedInputGroup"));
+//     $(this).closest(".addedInputGroup").remove();
+// }
+
