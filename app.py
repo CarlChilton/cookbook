6157251b54d099a9c7b6d9bdf12c8263b63741bb
Carl Chilton <carlchilton@yahoo.co.uk>
2019-02-22 17:26:22 +0000

    Successfully adding data to the database via the front end

--- app.py
+++ app.py
@@ -20,6 +20,33 @@ def get_recipes():
 def add_recipe(): 
     return render_template("add-recipe.html", allergens=mongo.db.allergens.find(), lifestyle=mongo.db.lifestyle.find())
 
+@app.route('/insert_recipe', methods=['POST'])
+def insert_recipe():
+    form_data = request.form.copy()
+    ingredients=[];
+    instructions=[];
+    allergens=[];
+    for field in request.form:
+        if field[:-1] == "ingredient":
+            ingredients.append(form_data[field])
+            del form_data[field]
+        if field[:-1] == "instruction":
+            instructions.append(form_data[field])
+            del form_data[field]
+        if field[:8] == "checkbox":
+            allergens.append(field[8:])
+            del form_data[field]
+    
+    if len(ingredients) > 0:
+        form_data["ingredients"] = ingredients
+    if len(instructions) > 0:
+        form_data["instructions"] = instructions
+    if len(allergens) > 0:
+        form_data["allergens"] = allergens
+    
+    recipes = mongo.db.recipes
+    recipes.insert_one(form_data.to_dict())
+    return redirect(url_for('get_recipes'))
 
 if __name__ == "__main__":
     app.run(host=os.getenv("IP"), 
--- static/js/script.js
+++ static/js/script.js
@@ -1,16 +1,25 @@
 $(document).ready(function() {
-    
+    $("#ingredientsList").find(".ingredient")
+});
+
+$("#addRecipeForm").on("submit", function(e) {
+    e.preventDefault();
+    console.log("Stopped it");
+    return false;
 });
 
 function add_ingredient() {
-    let ingredients = ($("#ingredientsList").html());
     let totalIngredients = $("#ingredientsList").find(".form-control").length + 1;
-    $("#ingredientsList").append("<label class='stepLabel' for='instruction" + totalIngredients + "'>Ingredient " 
+    $("#ingredientsList").append("<label class='stepLabel' for='ingredient" + totalIngredients + "'>Ingredient " 
     + totalIngredients + "</label><input id='ingredient" + totalIngredients
     + "' name='ingredient" + totalIngredients
     + "' type='text' class='form-control'>");
 }
-    
-    function add_cooking_step() {
-        
-    }
+
+function add_instruction() {
+    let totalInstructions = $("#instructionsList").find(".form-control").length + 1;
+    $("#instructionsList").append("<label class='stepLabel' for='instruction" + totalInstructions + "'>Step " 
+    + totalInstructions + "</label><input id='ingredient" + totalInstructions
+    + "' name='instruction" + totalInstructions
+    + "' type='text' class='form-control'>");
+}
