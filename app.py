7467d811f022aee95c6e92f4732301615bcd0d9c
Carl Chilton <carlchilton@yahoo.co.uk>
2019-02-22 11:51:13 +0000

    Database connected and pulling data into app

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 import os
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = "recipes"
+app.config["MONGO_URI"] = "mongodb://admin:password1@ds147225.mlab.com:47225/recipes"
+
+mongo = PyMongo(app)
+
 @app.route("/")
-def home():
-    return render_template("base.html")
+@app.route('/get_recipes')
+def get_recipes():
+    return render_template("recipes.html", 
+    recipes=mongo.db.recipes.find())
 
 
 if __name__ == "__main__":
